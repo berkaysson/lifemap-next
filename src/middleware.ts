62e479d2031c9a341c0937d96b0a0cb319eb6838
berkaysson <berkaysonel85@gmail.com>
2024-07-16 13:56:34 +0300

    update middleware for redirect user to login when is trying to enter protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,21 +17,15 @@ export default auth((req) => {
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-  if (isApiAuthRoute) {
-    // return null;
-  }
-
   if (isAuthRoute) {
     if (isLoggedIn) {
       return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
     }
   }
 
-  if (!isLoggedIn && !isPublicRoute) {
-    // return null;
+  if (!isLoggedIn && !isPublicRoute && !isAuthRoute) {
+    return Response.redirect(new URL("/auth/login", nextUrl));
   }
-
-  // return null;
 });
 
 export const config = {
