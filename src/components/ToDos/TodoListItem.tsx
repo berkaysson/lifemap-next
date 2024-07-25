5bdc1b21e346121163049b2d3c3623f0de6f8bfa
berkaysson <berkaysonel85@gmail.com>
2024-07-25 22:40:38 +0300

    add isExpired to todo

--- src/components/ToDos/TodoListItem.tsx
+++ src/components/ToDos/TodoListItem.tsx
@@ -21,6 +21,8 @@ const TodoListItem = ({
 }) => {
   const { onDeleteTodo, onUpdateTodo } = useContext(TodoContext);
 
+  const isExpired = todo.endDate < new Date();
+
   const handleDelete = async () => {
     await onDeleteTodo(todo.id);
   };
@@ -31,19 +33,20 @@ const TodoListItem = ({
   };
 
   return (
-    <li className="flex flex-col gap-2 p-4 border-b">
-      <div
-        className={`flex flex-row gap-2  ${
-          todo.completed ? "line-through" : ""
-        }`}
-      >
-        <span>{todo.name}</span>
-        <span>{todo.colorCode}</span>
-        <span>{todo.completed}</span>
-        <span>{todo.startDate.toISOString().slice(0, 10)}</span>/
+    <li
+      className="flex flex-col gap-2 p-4 border-b"
+    >
+      <div className="flex flex-col gap-2">
+        <div>
+          <span className="mr-2 text-xl">
+            {todo.completed ? "🟢" : isExpired ? "🔴" : "🟡"}
+          </span>
+          <span>{todo.name}</span>
+          <span>{todo.colorCode}</span>
+        </div>
+        <div>{todo.description}</div>
         <span>{todo.endDate.toISOString().slice(0, 10)}</span>
       </div>
-      <div>{todo.description}</div>
       <div className="flex flex-row gap-2">
         <Button variant={"outline"} size={"sm"} onClick={handleComplete}>
           {todo.completed ? "Uncomplete" : "Complete"}
