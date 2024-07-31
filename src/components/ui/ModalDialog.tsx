ed84a70b1a20d06dec0b9478b7d8b466651249d4
berkaysson <berkaysonel85@gmail.com>
2024-07-31 16:25:59 +0300

    add modal dialog component

---
+++ src/components/ui/ModalDialog.tsx
@@ -0,0 +1,52 @@
+import { DialogDescription, DialogTitle } from "@radix-ui/react-dialog";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTrigger,
+} from "./dialog";
+import { Button } from "./button";
+
+interface ModalDialogProps {
+  triggerButton: JSX.Element;
+  title: string;
+  description?: string;
+  children?: React.ReactNode;
+  isOpen?: boolean;
+  setIsOpen?: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const ModalDialog = ({
+  triggerButton,
+  title,
+  description,
+  children,
+  isOpen,
+  setIsOpen,
+}: ModalDialogProps) => {
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger asChild>{triggerButton}</DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+
+        {children}
+
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button type="button" variant={"destructive"}>
+              Cancel
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default ModalDialog;
