333868509e168b0e6d7b07a806db37564b79f47e
berkaysson <berkaysonel85@gmail.com>
2024-08-02 19:41:07 +0300

    create ColorCircle component

--- src/components/ui/ColorCircle.tsx
+++ src/components/ui/ColorCircle.tsx
@@ -2,7 +2,7 @@ const ColorCircle = ({ colorCode = "darkblue" }: { colorCode: string }) => {
   return (
     <span
       style={{ backgroundColor: colorCode }}
-      className="inline-block rounded-full w-6 h-6"
+      className="inline-block rounded-full w-6 h-6 border shadow-sm border-slate-400"
     />
   );
 };
