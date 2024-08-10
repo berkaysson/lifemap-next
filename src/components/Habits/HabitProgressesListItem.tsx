8df763ed95efca2801ccdcbc4c59fa4f5d8b78f5
berkaysson <berkaysonel85@gmail.com>
2024-08-10 20:44:00 +0300

    fix habit progress items wrong imlementation of IsCompleted

--- src/components/Habits/HabitProgressesListItem.tsx
+++ src/components/Habits/HabitProgressesListItem.tsx
@@ -3,13 +3,15 @@ import { HabitProgress } from "@prisma/client";
 import IsCompleted from "../ui/IsCompleted";
 
 const HabitProgressesListItem = ({ progress }: { progress: HabitProgress }) => {
+  const isExpired = progress.endDate && new Date() > new Date(progress.endDate);
+
   return (
     <li className="border rounded-sm p-2 space-y-1">
       <div>
         {formatDate(progress.startDate)} - {formatDate(progress.endDate)}
       </div>
       <div className="flex flex-row gap-2">
-        <IsCompleted isCompleted={progress.completed} />
+        <IsCompleted isCompleted={progress.completed} isExpired={isExpired} />
         <h2 className="font-bold">{progress.order}. Repeat</h2>
       </div>
 
