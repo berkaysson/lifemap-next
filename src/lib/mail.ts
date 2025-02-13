407eefb2aaa29ac1c5668959ee791da7240f77a1
Berkay Sonel <berkaysonel85@gmail.com>
2025-02-13 16:08:53 +0300

    refactor: remove hardcoded email sender and use direct email address

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -3,14 +3,12 @@ import { Resend } from "resend";
 
 // Initialize Resend with API key from environment variables
 const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = "habivita.com";
-
 // Send verification email
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${MAIN_DOMAIN}/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
-    from: fromEmail,
+    from: "mail@habivita.com",
     to: email,
     subject: "Confirm your email",
     html: `<p>Click <a href='${confirmLink}'>here</a> to confirm email.</p>`,
@@ -22,7 +20,7 @@ export const sendResetPasswordEmail = async (email: string, token: string) => {
   const resetLink = `${MAIN_DOMAIN}/auth/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: fromEmail,
+    from: "mail@habivita.com",
     to: email,
     subject: "Reset your email",
     html: `<p>Click <a href='${resetLink}'>here</a> to reset password.</p>`,
